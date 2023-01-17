a6524da93221588542edeaf3173dd5c484f09146
ITSC <ITSC@gmail.com>
2023-01-17 21:50:21 +0900

    day02

--- src/day01/MainClass02.java
+++ src/day01/MainClass02.java
@@ -8,9 +8,13 @@ package day01;
 //	(한줄주석)자바 코드 시작 class입니다.
 //	자바 코드는  class예약어 {	}안에 정의
 public class MainClass02 {
-	// 메인 메소드 - 자바 처음 실행되는 메소드
+	/**
+	 * 	JavaDoc  주석
+	 * 	메인 메소드 - 자바 처음 실행되는 메소드
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// 
+		// Console 출력 명령어
 		System.out.println("안녕하세요");
 	}
 
