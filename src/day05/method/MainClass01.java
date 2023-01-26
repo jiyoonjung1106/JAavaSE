c733b7f91e84df2348a71368f1e5a070f4a03d94
ITSC <ITSC@gmail.com>
2023-01-26 21:47:57 +0900

    java study

--- src/day05/method/MainClass01.java
+++ src/day05/method/MainClass01.java
@@ -9,16 +9,35 @@ package day05.method;
  *		return 반환값;		 //void일때 생략가능 
  * 
  * }
+ * 	(면접때 많이 물어봄)오버로딩(overloading)
+ * 		메소드 명이 같아도, 매개변수 개수 또는 타입이 다르면 사용가능.
  * 
  */
 public class MainClass01 {
 	public static void main(String[] args) {
 			// hello 메소드 호출하기
 			hello();
+			
+			// printName  메소드 호출하기
+			 printName("파치리스");
+			 printName("피카츄", "파치리스");
+			 printName(1000);
 	}
-	
+	 // 인자 X 리턴 X -> 호출시 실행하고 끝나는 메소드
 	 public static void hello() {
 		 System.out.println("Hello, World");
 	 }
-
+	 // 인자 0 리턴 X
+	 public static void printName(String name) {
+		 System.out.println("이름 : "  +name);
+	 }
+	 // 오버로딩(overloading)
+	 public static void printName(String firstName, String lastName) {
+		 System.out.println(" 이름 : "+ lastName+ firstName);
+	 }
+	 // 오버로딩(overloading)
+	// 인자 0 리턴 X
+	 public static void printName(int name) {
+		 System.out.println("로봇 번호 : "  +name);
+	 }
 }
