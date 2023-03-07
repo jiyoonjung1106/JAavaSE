ce06cd91f6db9925a331498d1388b15de5ea4eb4
Choppysheep4370 <plan2031@naver.com>
2023-03-07 19:55:22 +0900

    java_acedemy_study

---
+++ src/day14/jdbc/MainClass05.java
@@ -0,0 +1,48 @@
+package day14.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MainClass05 {
+	public static void main(String[] args) throws SQLException {
+		Connection conn = null;
+		// Statement 사용하지 말고 PreparedStatement 사용하자!!!
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			// 1. 드라이버 클래스 정보 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			// 2. 데이터베이스 연결
+			String url ="jdbc:oracle:thin:@localhost:21521:xe";
+			conn = DriverManager.getConnection(url, "hr", "hr");
+			
+			StringBuffer sql = new StringBuffer();
+			sql.append("UPDATE sales_reps SET ");
+			sql.append("name = '리자몽' ");
+			sql.append("WHERE id = ? ");
+						
+			pstmt = conn.prepareStatement(sql.toString());
+			
+			pstmt.setInt(1, 7);
+			
+			int result =  pstmt.executeUpdate();
+			
+			System.out.println("result : " + result);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null)rs.close();
+			if(pstmt != null)pstmt.close();
+			if(conn != null)conn.close();
+		}
+		
+	}
+
+}
