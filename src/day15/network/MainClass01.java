f26e68d42ff0bef3d2741085446c8bd9b74a7728
Choppysheep4370 <plan2031@naver.com>
2023-03-09 21:20:06 +0900

    network

---
+++ src/day15/network/MainClass01.java
@@ -0,0 +1,27 @@
+package day15.network;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class MainClass01 {
+	public static void main(String[] args) throws UnknownHostException {
+		//InetAddress는 IP를 관리하는 클래스
+		//getLocalHost() 로컬 시스템의 IP를 InetAddress 객체로 변환
+		InetAddress ia = InetAddress.getLocalHost();
+		
+		//getHostName()는 호스트 이름을 반환
+		System.out.println("host name : "+ia.getHostName());
+		
+		//getHostAddress()는 호스트IP를 반환
+		System.out.println("host ip: "+ia.getHostAddress());
+		
+		//getByName()는 특정사이트의 IP를 InetAddress로 반환
+		ia = InetAddress.getByName("www.naver.com");
+		
+		System.out.println("hostname:"+ia.getHostName());
+		System.out.println("host ip:"+ia.getHostAddress());
+		
+		
+	}
+
+}
